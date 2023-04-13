7ebb3da01d3c3e929c51cb5e45d2d5a0765e89d0
Himmatlal Parmar <himmatlalparmar785@gmail.com>
2023-04-13 17:59:30 +0530

    create banner section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from "react";
+import Banner from "./Componants/Banner";
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <Banner/>
+    </>
   );
 }
 
