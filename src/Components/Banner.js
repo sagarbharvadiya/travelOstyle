038065cede09346ff7b90feb17a5c3c82b232b9d
Himmatlal Parmar <himmatlalparmar785@gmail.com>
2023-04-14 18:33:15 +0530

    changes in banner and tourcard

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -31,7 +31,7 @@ function Banner(){
                                 <div className="banner-right-image">
                                     <img src={banner_world_image}/>
                                 </div>
-                                <h2>style</h2>
+                                <h2>Style</h2>
                             </div>
                         </div>
                     </div>
@@ -42,51 +42,3 @@ function Banner(){
 }
 
 export default Banner;
-
-
-
-
-
-
-// import React from "react";
-// import banner2 from '../images/banner-image2.webp';
-
-// function Banner() {
-//   return (
-//     <>
-//       <div className="banner-section">
-//         <div className="banner-wrapper">
-//           <div className="banner-container">
-//             <div className="banner-image">
-//               <img src={banner2} alt="banner"/>
-//             </div>
-//             <div className="banner-title-folder">
-//               <div className="banner-left-section">
-//                 <h2 className="banner-title">Travel</h2>
-//                 <div className="banner-des-conatiner">
-//                   <div className="banner-des-folder1">
-//                     <h2>01</h2>
-//                   </div>
-//                   <div className="banner-des-folder2">
-//                     <h2>DISCOVER TOURS</h2>
-//                     <p>
-//                       internatios is international, non profit, professional
-//                       organization founded by a group of Transactional Analysts
-//                       engaged in applying their knowledge and skills for
-//                       promoting global
-//                     </p>
-//                   </div>
-//                 </div>
-//               </div>
-//               <div className="banner-right-section">
-//                 <h2>O style</h2>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Banner;
