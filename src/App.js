ed0a443cb7cc7ceea4fdd4d4a8db395482ac9cf7
omkar213 <omkardhumal213@gmail.com>
2023-04-22 13:59:32 +0530

    Tour details page created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import TourCard from "./Components/TourCard";
-import About from "./Components/About";
-import Banner from './Components/Banner';
-import ContactUs from "./Components/ContactUs";
-import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import './css/aos.css';
-
+import "./css/aos.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TourcardDetails from "./Pages/TourcardDetails";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <>
-      <Header/>
-      <Banner/>
-      <TourCard />
-      <About />
-      <ContactUs/>
-      <Footer/>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tour-details/:slug" element={<TourcardDetails />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -82,92 +82,101 @@
 
 // export default Banner;
 
-
 import React, { useState, useEffect } from "react";
-import banner_mobile from '../images/banner-mobile-image.jpg';
-import travelImage from '../images/ezgif.com-crop (1).gif'
-import client from '../Client';
-import Aos from 'aos'
+import banner_mobile from "../images/banner-mobile-image.jpg";
+import travelImage from "../images/ezgif.com-crop (1).gif";
+import client from "../Client";
+import Aos from "aos";
 import NewBanner from "./NewBanner";
-import banner_video from "../assets/video/pexels.mp4"
+import banner_video from "../assets/video/pexels.mp4";
 // import ReactCurtain from "./ReactCurtain";
 
 function Banner() {
+  const [checked, setChecked] = useState(true);
 
-    const [checked, setChecked] = useState(true);
+  const [entry, setEntry] = useState([]);
 
-    const [entry, setEntry] = useState([]);
-
-    useEffect(() => {
-        const fetchPage = async () => {
-            try {
-                const response = await client.getEntries({
-                    content_type: "banner",
-                });
-                console.log()
-                console.log(response)
-                if (response.items.length) {
-                    setEntry(response.items);
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        fetchPage();
-        Aos.init({ duration: 1500 });
-    }, []);
-    return (
-        <>
-            {
-                entry.map((item) => {
-                    const { title, description, leftTitle, linkField, rightTitle } = item.fields;
-                    const imageUrl = (item.fields.image.fields.file.url);
-                    const id = item.sys.id
-                    return (
-                        <React.Fragment key={id}>
-                            <div className="banner-section">
-                                <div className="banner-wrapper curtain">
-                                    <div className="banner-container curtain__wrapper">
-                                        <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}/>
-                                        {/* <div className="banner-image curtain__content">
+  useEffect(() => {
+    const fetchPage = async () => {
+      try {
+        const response = await client.getEntries({
+          content_type: "banner",
+        });
+        // console.log();
+        // console.log(response);
+        if (response.items.length) {
+          setEntry(response.items);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPage();
+    Aos.init({ duration: 1500 });
+  }, []);
+  return (
+    <>
+      {entry.map((item) => {
+        const { title, description, leftTitle, linkField, rightTitle } =
+          item.fields;
+        const imageUrl = item.fields.image.fields.file.url;
+        const id = item.sys.id;
+        return (
+          <React.Fragment key={id}>
+            <div className="banner-section">
+              <div className="banner-wrapper curtain">
+                <div className="banner-container curtain__wrapper">
+                  <input
+                    type="checkbox"
+                    checked={checked}
+                    onChange={() => setChecked(!checked)}
+                  />
+                  {/* <div className="banner-image curtain__content">
                                             <img src={imageUrl} alt="banner-image" />
                                         </div> */}
-                                        {/* <video className="banner-image curtain__content" width="100" height="100" controls>
+                  {/* <video className="banner-image curtain__content" width="100" height="100" controls>
                                             <source src="images/video2.mp4" type="video/mp4"/>
                                         </video> */}
-                                        <video className="banner-image curtain__content" controls="controls" autoplay="autoplay" muted="muted" loop>
-                                            <source src={banner_video} type="video/mp4"/>
-                                        </video>
-                                        <div className="banner-image banner-mobile urtain__content">
-                                            <img src={banner_mobile} alt="banner-image" />                                         </div>
-                                        <div className="banner-left-section curtain__panel curtain__panel--left">
-                                            <h2 className="banner-title">{leftTitle}</h2>
-                                            <div className="banner-des-conatiner">
-                                                <div className="banner-des-folder2">
-                                                    <h2>{title}</h2>
-                                                    <p>{description}</p>
-                                                    <a href="#">{linkField}</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="banner-right-image">
-                                            <img src={travelImage} alt=""></img>
-                                        </div>
-                                        <div className="banner-right-section curtain__panel curtain__panel--right">
-                                            <h2>{rightTitle}</h2>
-                                        </div>
-                                    </div>
-                                    <a href="#scroll-down" className="scroll-arrow"><i class="fa-solid fa-angle-down"></i></a>
-                                </div>
-                            </div>
-                            <NewBanner />
-                        </React.Fragment>
-
-                    )
-                })
-            }
-        </>
-    )
+                  <video
+                    className="banner-image curtain__content"
+                    controls="controls"
+                    autoplay="autoplay"
+                    muted="muted"
+                    loop
+                  >
+                    <source src={banner_video} type="video/mp4" />
+                  </video>
+                  <div className="banner-image banner-mobile urtain__content">
+                    <img src={banner_mobile} alt="banner-image" />{" "}
+                  </div>
+                  <div className="banner-left-section curtain__panel curtain__panel--left">
+                    <h2 className="banner-title">{leftTitle}</h2>
+                    <div className="banner-des-conatiner">
+                      <div className="banner-des-folder2">
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                        <a href="#">{linkField}</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="banner-right-image">
+                    <img src={travelImage} alt=""></img>
+                  </div>
+                  <div className="banner-right-section curtain__panel curtain__panel--right">
+                    <h2>{rightTitle}</h2>
+                  </div>
+                </div>
+                <a href="#scroll-down" className="scroll-arrow">
+                  <i class="fa-solid fa-angle-down"></i>
+                </a>
+              </div>
+            </div>
+            <NewBanner />
+          </React.Fragment>
+        );
+      })}
+    </>
+  );
 }
 
 export default Banner;
