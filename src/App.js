6bca54c177029d3f25f8fe3dd5f52b9e809ef301
omkar213 <omkardhumal213@gmail.com>
2023-04-24 13:41:29 +0530

    added unique class in page with slug

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "./Components/Footer";
 import "./css/aos.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import TourcardDetails from "./Pages/TourcardDetails";
 import Home from "./Pages/Home";
 import Header from "./Components/Header";
 
 function App() {
+  const location = useLocation();
+  const activeSlug = location.pathname.split("/").pop(); // Get the last part of the URL path
+  useEffect(() => {
+    document.body.classList.add(`page-${activeSlug}`); // Add the class to the body tag
+    return () => {
+      document.body.classList.remove(`page-${activeSlug}`); // Remove the class from the body tag when the component unmounts
+    };
+  }, [activeSlug]);
+
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tour-details/:slug" element={<TourcardDetails />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tour-details/:slug" element={<TourcardDetails />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
