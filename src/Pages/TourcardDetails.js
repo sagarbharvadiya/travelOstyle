33efe4ce754923b4c5d441b19d6a64d6dbc5644d
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-05-10 19:17:24 +0530

    tourcard details

--- src/Pages/TourcardDetails.js
+++ src/Pages/TourcardDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import detailImg from "../images/detailspageimg1.jpg";
 import placeImg from "../images/londonbridge.jpg";
+import map from '../images/map.jpg'
 import "../css/pages.css";
 import { useParams } from "react-router-dom";
 import client from "../Client";
@@ -8,6 +9,9 @@ import client from "../Client";
 const TourcardDetails = () => {
   const { slug } = useParams();
   const [entry, setEntry] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);;
   useEffect(() => {
     const fetchdetailsPage = async () => {
       try {
@@ -27,7 +31,7 @@ const TourcardDetails = () => {
   }, [slug]);
 
   return (
-    <div className="page-container" tabIndex={0}>
+    <div className="" tabIndex={0}>
       <div>
         {entry &&
           entry.map((info) => {
@@ -36,106 +40,106 @@ const TourcardDetails = () => {
             const id = info.fields.sys;
             return (
               <>
+
                 <div className="tour-details-container" key={id}>
-                  <div className="card-details">
-                    <h1>{packageTitle}</h1>
-                    <span>$ {packageStartingPrice}</span>
-                    <img
-                      className="content-img"
-                      src={detailImg}
-                      alt="man-standing-besides-church"
-                    />
-                    <h2 className="content-heading">About Itinerary</h2>
-                    <p className="content-info">
-                      It is a long established fact that a reader will be
-                      distracted by the readable content of a page when looking
-                      at its layout. The point of using Lorem Ipsum is that it
-                      has a more-or-less normal distribution of letters, as
-                      opposed to using 'Content here, content here', making it
-                      look like readable English. Many desktop publishing
-                      packages and web page editors now use Lorem Ipsum as their
-                      default model text, and a search for 'lorem ipsum' will
-                      uncover many web sites still in their infancy.
-                    </p>
-                    <button className="tour-book-btn">Book Now</button>
-                    <button className="tour-query-btn">Submit Query</button>
-                  </div>
-                </div>
-
-                <div className="tour-map">
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.288961634382!2d-0.0903025740590879!3d51.50791446066668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876035159bb13c5%3A0xa61e28267c3563ac!2sLondon%20Bridge!5e0!3m2!1sen!2sin!4v1681972116941!5m2!1sen!2sin"
-                    width="100%"
-                    height="400"
-                    style={{ border: 0 }}
-                    allowfullscreen=""
-                    loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"
-                    title="london bridge"
-                  ></iframe>
-                </div>
-
-                <div className="timeline-container">
-                  {itineraries &&
-                    itineraries.map((itenary, index) => {
-                      const { title, localLunch, description } = itenary.fields;
-                      const meal = localLunch ? (
-                        <li>Local Lunch</li>
-                      ) : (
-                        <>
-                          <li>Breakfast</li>
-                          <li>Dinner</li>
-                        </>
-                      );
-                      //code for extracting name of place
-                      const titleParts = title.split(":");
-                      const placeName =
-                        titleParts.length > 1 ? titleParts[1].trim() : "";
-
-                      //code for extracting day
-                      const dayNum =
-                        titleParts.length > 1 ? titleParts[0].trim() : "";
-                      return (
-                        <div className="timeline-div">
-                          <div className="timeline-left-div">
-                            <div className="sticky-top">
-                              <span className="day-details-btn">{dayNum}</span>
+                  <img
+                    className="content-img"
+                    src={detailImg}
+                    alt="man-standing-besides-church"
+                  />
+                  <div className="page-container">
+                    <div className="card-details">
+                      <h1>{packageTitle}</h1>
+                      <span>$ {packageStartingPrice}</span>
+                      {/* <h2 className="content-heading">About Itinerary</h2> */}
+                      <p className="content-info">
+                        It is a long established fact that a reader will be
+                        distracted by the readable content of a page when looking
+                        at its layout. The point of using Lorem Ipsum is that it
+                        has a more-or-less normal distribution of letters, as
+                        opposed to using 'Content here, content here', making it
+                        look like readable English. Many desktop publishing
+                        packages and web page editors now use Lorem Ipsum as their
+                        default model text, and a search for 'lorem ipsum' will
+                        uncover many web sites still in their infancy.
+                      </p>
+                      <div className="btn">
+                        <button className="tour-book-btn">Book Now</button>
+                        <button className="tour-query-btn" onClick={handleOpen}>
+                          Submit Query
+                        </button>
+                        {isOpen && (
+                          <div className="popup">
+                            <div className="popup-content">
+                              <button className="close-btn" onClick={handleClose}>
+                                &times;
+                              </button>
+                                <h1>{packageTitle}</h1>
+                                <span>$ {packageStartingPrice}</span>
+                              <form> 
+                                <label htmlFor="name">Name:</label>
+                                <input type="text" id="name" name="name" required />
+                                <label htmlFor="email">Email:</label>
+                                <input type="email" id="email" name="email" required />
+                                <label htmlFor="message">Message:</label>
+                                <textarea id="message" name="message" required></textarea>
+                                <button type="submit">Submit</button>
+                              </form>
                             </div>
                           </div>
-                          <div className="timeline-right-div">
-                            <div className="day-details-box">
-                              <div className="day-info">
-                                <h4>{placeName}</h4>
-                                <div className="tour-details-star-folder">
-                                  <a href="/">
-                                    <i class="fa-solid fa-star"></i>
-                                  </a>
-                                  <a href="/">
-                                    <i class="fa-solid fa-star"></i>
-                                  </a>
-                                  <a href="/">
-                                    <i class="fa-solid fa-star"></i>
-                                  </a>
-                                  <a href="/">
-                                    <i class="fa-solid fa-star"></i>
-                                  </a>
-                                  <a href="/">
-                                    <i class="fa-solid fa-star"></i>
-                                  </a>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="tour-map">
+                      <img src={map} alt="map"></img>
+                    </div>
+                    <div className="timeline-container">
+                      {itineraries &&
+                        itineraries.map((itenary, index) => {
+                          const { title, localLunch, description } = itenary.fields;
+                          const meal = localLunch ? (
+                            <li>Local Lunch</li>
+                          ) : (
+                            <>
+                              <li>Breakfast</li>
+                              <li>Dinner</li>
+                            </>
+                          );
+                          //code for extracting name of place
+                          const titleParts = title.split(":");
+                          const placeName =
+                            titleParts.length > 1 ? titleParts[1].trim() : "";
+
+                          //code for extracting day
+                          const dayNum =
+                            titleParts.length > 1 ? titleParts[0].trim() : "";
+                          return (
+                            <div className="timeline-div">
+                              <div className="timeline-left-div">
+                                <div className="sticky-top">
+                                  <span className="day-details-btn">{dayNum}</span>
                                 </div>
-                                {description.content.map((desc, index1) => {
-                                  return <p>{desc.content[0].value}</p>;
-                                })}
-                                <ul>{meal}</ul>
                               </div>
-                              <div className="place-img">
-                                <img src={placeImg} alt="London-bridge" />
+                              <div className="timeline-right-div">
+                                <div className="day-details-box">
+                                  <div className="day-info">
+                                    <h4>{placeName}</h4>
+                                    {description.content.map((desc, index1) => {
+                                      return <p>{desc.content[0].value}</p>;
+                                    })}
+                                    <ul>{meal}</ul>
+                                  </div>
+                                  <div className="place-img">
+                                    <img src={placeImg} alt="London-bridge" />
+                                  </div>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        </div>
-                      );
-                    })}
+                          );
+                        })}
+                    </div>
+                  </div>
                 </div>
               </>
             );
