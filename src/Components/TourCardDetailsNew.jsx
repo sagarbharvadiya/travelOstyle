824208b5761f5d6ff6b3980079b8e5e82640fdbc
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-05-17 18:31:43 +0530

    new banner section

---
+++ src/Components/TourCardDetailsNew.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const TourCardDetailsNew = () => {
+    return (
+        <>
+            <section className='Tour_details'>
+                <div className='nav_bar'>
+                    <div className='container'>
+                        <div className='d_flex'>
+                            <div className='nav_title'>
+                                <h2 className='tour_title'>
+                                    Brazil
+                                </h2>
+                            </div>
+
+                            <span className='tour_nav'>
+                                <a href="/">Main information</a>
+                                <a href="/">Map</a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className='container'>
+                    <div className='tour-left-content'>
+                        <div className='left_wrapper'>
+                            
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default TourCardDetailsNew
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,11 +3,13 @@ import Banner from "../Components/Banner";
 import TourCard from "../Components/TourCard";
 import About from "../Components/About";
 import ContactUs from "../Components/ContactUs";
+import BannerNew from "../Components/BannerNew";
 
 const Home = () => {
   return (
     <>
-      <Banner />
+      {/* <Banner /> */}
+      <BannerNew/>
       <TourCard />
       <About />
       <ContactUs />
